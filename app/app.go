a4967067838e9f05e535be81202314bc67eb3c07
psude <paulo.suderio@bndes.gov.br>
2020-12-30 19:53:44 -0300

    Comandos up, down e set

--- app/app.go
+++ app/app.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"errors"
+	"os"
 	"regexp"
 	"strconv"
+	"text/template"
 )
 
 var valid = regexp.MustCompile(`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
@@ -17,27 +19,123 @@ type Semver struct {
 	Build   string
 }
 
+// Show prints the semantic version
+func Show(semver Semver, verbose bool, linebreak bool) {
+	var templ string
+	if verbose {
+		if linebreak {
+			templ = "Major: {{ .Major}}\nMinor: {{ .Minor}}\nPatch: {{ .Patch}}\n{{if .Release}}Release: {{ .Release}}\n{{end}}{{if .Build}}Build: {{ .Build}}\n{{end}}"
+		} else {
+			templ = "Major: {{ .Major}} Minor: {{ .Minor}} Patch: {{ .Patch}} {{if .Release}}Release: {{ .Release}}{{end}} {{if .Build}}Build: {{ .Build}}{{end}}\n"
+		}
+	} else {
+		if linebreak {
+			templ = "{{ .Major}}\n{{ .Minor}}\n{{ .Patch}}\n{{if .Release}}{{ .Release}}\n{{end}}{{if .Build}}{{ .Build}}\n{{end}}"
+		} else {
+			templ = "{{ .Major}}.{{ .Minor}}.{{ .Patch}}{{if .Release}}-{{ .Release}}{{end}}{{if .Build}}+{{ .Build}}{{end}}\n"
+		}
+	}
+
+	t, err := template.New("semver").Parse(templ)
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(os.Stdout, semver)
+	if err != nil {
+		panic(err)
+	}
+}
+
 // ParseSemVer parses a string and returns its semantic versioning parts
 func ParseSemVer(semver string) (Semver, error) {
 	var result = valid.FindStringSubmatch(semver)
 	if result == nil || len(result) != 6 {
 		return Semver{}, errors.New("semver not found")
 	}
-	major, err := strconv.Atoi(result[1])
-	if err != nil {
-		return Semver{}, errors.New("major version is not a number")
-	}
-	minor, err := strconv.Atoi(result[2])
-	if err != nil {
-		return Semver{}, errors.New("minor version is not a number")
-	}
-	patch, err := strconv.Atoi(result[3])
-	if err != nil {
-		return Semver{}, errors.New("patch version is not a number")
-	}
+	major, _ := strconv.Atoi(result[1])
+	minor, _ := strconv.Atoi(result[2])
+	patch, _ := strconv.Atoi(result[3])
 	release := result[4]
-
 	build := result[5]
-
 	return Semver{major, minor, patch, release, build}, nil
+}
+
+// Up increases a semantic version
+func Up(semver string, major bool, minor bool) (Semver, error) {
+	oldVer, err := ParseSemVer(semver)
+	if err != nil {
+		return Semver{}, err
+	}
+	if major {
+		oldVer.Major++
+		oldVer.Minor = 0
+		oldVer.Patch = 0
+	} else if minor {
+		oldVer.Minor++
+		oldVer.Patch = 0
+	} else {
+		oldVer.Patch++
+	}
+	oldVer.Release = ""
+	oldVer.Build = ""
+	return oldVer, nil
+}
+
+// Down increases a semantic version
+func Down(semver string, major bool, minor bool) (Semver, error) {
+	oldVer, err := ParseSemVer(semver)
+	if err != nil {
+		return Semver{}, err
+	}
+	if major {
+		oldVer.Major--
+		oldVer.Minor = 0
+		oldVer.Patch = 0
+	} else if minor {
+		oldVer.Minor--
+		oldVer.Patch = 0
+	} else {
+		oldVer.Patch--
+	}
+	oldVer.Release = ""
+	oldVer.Build = ""
+	return oldVer, nil
+}
+
+// Set sets a semantic version
+func Set(semver string, major string, minor string, patch string, release string, build string) (Semver, error) {
+	oldVer, err := ParseSemVer(semver)
+	if err != nil {
+		return Semver{}, err
+	}
+
+	if len(major) > 0 {
+		oldVer.Major, err = strconv.Atoi(major)
+		if err != nil {
+			return Semver{}, err
+		}
+	}
+	if len(minor) > 0 {
+		oldVer.Minor, err = strconv.Atoi(minor)
+		if err != nil {
+			return Semver{}, err
+		}
+	}
+	if len(patch) > 0 {
+		oldVer.Patch, err = strconv.Atoi(patch)
+		if err != nil {
+			return Semver{}, err
+		}
+	}
+	if len(major) > 0 {
+		oldVer.Release = release
+	}
+	if len(major) > 0 {
+
+		oldVer.Build = build
+	}
+
+	// TODO validar o resultado
+
+	return oldVer, nil
 }
--- parser/parser.go
+++ parser/parser.go
@@ -17,15 +17,98 @@ func Parse() {
 
 	commando.
 		Register(nil).
-		AddArgument("semver", "Semantic Version", "").
+		SetShortDescription("Parses the semantic version").
+		SetDescription("This command shows the Major, Minor, Patch, Release and Build fields of the informed version.").
+		AddArgument("semver", "The semantic version", "").
+		AddFlag("verbose,v", "Shows field names", commando.Bool, false).
+		AddFlag("linebreak,l", "Shows one field per line", commando.Bool, false).
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
 			semver := args["semver"].Value
+			verbose, _ := flags["verbose"].GetBool()
+			linebreak, _ := flags["linebreak"].GetBool()
 			v, err := app.ParseSemVer(semver)
 			if err != nil {
 				fmt.Printf("%q is not a valid semantic version\n", semver)
 				return
 			}
-			fmt.Println(v.Major, v.Minor, v.Patch, v.Release, v.Build)
+			app.Show(v, verbose, linebreak)
+		})
+
+	commando.
+		Register("up").
+		SetShortDescription("Adds 1 to the version").
+		SetDescription("This command increases a semantic version by one, defaulting to the patch.").
+		AddArgument("semver", "The semantic version", "").
+		AddFlag("major,M", "Increases the major version", commando.Bool, false).
+		AddFlag("minor,m", "Increases the minor version", commando.Bool, false).
+		AddFlag("patch,p", "Increases the patch version", commando.Bool, true).
+		AddFlag("verbose,v", "Shows field names", commando.Bool, false).
+		AddFlag("linebreak,l", "Shows one field per line", commando.Bool, false).
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			semver := args["semver"].Value
+			major, _ := flags["major"].GetBool()
+			minor, _ := flags["minor"].GetBool()
+			verbose, _ := flags["verbose"].GetBool()
+			linebreak, _ := flags["linebreak"].GetBool()
+			v, err := app.Up(semver, major, minor)
+			if err != nil {
+				fmt.Printf("%q is not a valid semantic version\n", semver)
+				return
+			}
+			app.Show(v, verbose, linebreak)
+		})
+
+	commando.
+		Register("down").
+		SetShortDescription("Subtracts 1 to the version").
+		SetDescription("This command decreases a semantic version by one, defaulting to the patch.").
+		AddArgument("semver", "The semantic version", "").
+		AddFlag("major,M", "Decreases the major version", commando.Bool, false).
+		AddFlag("minor,m", "Decreases the minor version", commando.Bool, false).
+		AddFlag("patch,p", "Decreases the patch version", commando.Bool, true).
+		AddFlag("verbose,v", "Shows field names", commando.Bool, false).
+		AddFlag("linebreak,l", "Shows one field per line", commando.Bool, false).
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			semver := args["semver"].Value
+			major, _ := flags["major"].GetBool()
+			minor, _ := flags["minor"].GetBool()
+			verbose, _ := flags["verbose"].GetBool()
+			linebreak, _ := flags["linebreak"].GetBool()
+			v, err := app.Down(semver, major, minor)
+			if err != nil {
+				fmt.Printf("%q is not a valid semantic version\n", semver)
+				return
+			}
+			app.Show(v, verbose, linebreak)
+		})
+
+	commando.
+		Register("set").
+		SetShortDescription("Sets fields").
+		SetDescription("This command sets every field of the version.").
+		AddArgument("semver", "The semantic version", "").
+		AddFlag("major,M", "Decreases the major version", commando.String, "").
+		AddFlag("minor,m", "Decreases the minor version", commando.String, "").
+		AddFlag("patch,p", "Decreases the patch version", commando.String, "").
+		AddFlag("release,p", "Decreases the patch version", commando.String, "").
+		AddFlag("build,p", "Decreases the patch version", commando.String, "").
+		AddFlag("verbose,v", "Shows field names", commando.Bool, false).
+		AddFlag("linebreak,l", "Shows one field per line", commando.Bool, false).
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			semver := args["semver"].Value
+			major, _ := flags["major"].GetString()
+			minor, _ := flags["minor"].GetString()
+			patch, _ := flags["patch"].GetString()
+			release, _ := flags["release"].GetString()
+			build, _ := flags["build"].GetString()
+			verbose, _ := flags["verbose"].GetBool()
+			linebreak, _ := flags["linebreak"].GetBool()
+			v, err := app.Set(semver, major, minor, patch, release, build)
+			if err != nil {
+				fmt.Printf("%q is not a valid semantic version\n", semver)
+				return
+			}
+			app.Show(v, verbose, linebreak)
 		})
 	commando.Parse(nil)
 }
